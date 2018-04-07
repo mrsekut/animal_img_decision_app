62600af904f5a1b24dc3386b6de1b54630e4bc30
clomaru <k.cloudspider@gmail.com>
2018-04-07 19:10:16 +0900

    [Fix]

--- animal/animal_cnn.py
+++ animal/animal_cnn.py
@@ -51,7 +51,7 @@ def model_train(X_train, X_test, y_train, y_test):
     model.add(Activation('relu'))
     model.add(Conv2D(32, (3, 3)))
     model.add(Activation('relu'))
-    model.add(AveragePooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     model.add(Conv2D(64, (3, 3), padding='same'))
