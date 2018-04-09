5491c722eddab6c441823fb8fc4da9fa0bad72ed
clomaru <k.cloudspider@gmail.com>
2018-04-10 02:38:24 +0900

    [Add]Flaskの設定

---
+++ animal/predict_file.py
@@ -0,0 +1,67 @@
+import os
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+from keras.models import Sequential, load_model
+import keras, sys
+from PIL import Image
+import numpy as np
+
+classes = ["cat","penguin","hedgehog","snake"]
+num_classes = len(classes)
+image_size = 50
+
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['jpg','png','gif'])
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+# ファイルのアップロードの可否を判定する
+def allowed_file(filename):
+    # 正しい拡張子が指定されているか
+    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash("ファイルがありません")
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash("ファイルがありません")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+
+            # サニタイズ処理
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+            model = load_model('./animal_cnn_aug.h5')
+
+            image = Image.open(filepath)
+            image = image.convert("RGB")
+            image = image.resize((image_size, image_size))
+            data = np.array(image)
+            X = []
+            X.append(data)
+            X = np.array(X)
+
+            result = model.predict([X])[0]
+            predicted = result.argmax()
+            percentage = int(result[predicted] * 100)
+
+            return "ラベル: " + classes[predicted] + ", 確率:" + str(percentage) + "%"
+
+    return render_template('index.html')
+
+# @app.route('/uploads/<filename>')
+# def uploaded_file(filename):
+#     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
